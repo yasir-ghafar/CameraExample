c6e5eccf912e5339e14987ac81b35dc8955ed657
yasir-ghafar <yasir896@gmail.com>
2025-01-06 16:52:33 +0500

    compiled sdk updated.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.techlads.cameraexample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.techlads.cameraexample"
